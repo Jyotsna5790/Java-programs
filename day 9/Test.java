0060b6a46582217da055b7f619bc73b2d8bd825d
Jyotsna5790 <95307901+Jyotsna5790@users.noreply.github.com>
2021-12-28 19:25:46 +0530

    Add files via upload

---
+++ day 9/Test.java
@@ -0,0 +1,29 @@
+
+public class Test {
+	String name,roll;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRoll() {
+		return roll;
+	}
+
+	public void setRoll(String roll) {
+		this.roll = roll;
+	}
+	
+	
+	void display()
+	{
+		System.out.print("Name :"+name+" Rollno :"+roll);
+	}
+}
+
+
+
